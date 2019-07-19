461519c7e8d470816a48857111698018d224376e
Ivan <20475253+thevan4@users.noreply.github.com>
2019-07-19 21:57:26 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"./executessh"
+)
+
+func main() {
+	host := "1.1.1.1:22"
+	user := "admin4eg"
+	password := "pass"
+	shellPrompt := "#" //or $ for example
+	timeoutSeconds := 2
+
+	connection, err := executessh.Connect(host, user, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output, err := connection.SendCommands(shellPrompt, timeoutSeconds, "show hostname", "show interface mgmt 0 | json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(output)
+}
