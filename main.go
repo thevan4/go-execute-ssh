0b013592ff582193fb828ee1fd264cc78e25c0ae
Ivan Tihonov <4thevan@gmail.com>
2019-08-05 12:29:32 +0300

    add flags

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -12,20 +14,58 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+var logOutput, logLevel, logFormat, syslogTag string
+var host, user, password, shellPrompt, rawTimeoutForExecuteCommand, rawCommands, rawMaxBufferBytes string
+
+func init() {
+	flag.StringVar(&logOutput, "log-output", "stdout", "log output. Example: stdout")
+	flag.StringVar(&logLevel, "log-level", "info", "log level. Example: info")
+	flag.StringVar(&logFormat, "log-format", "default", "log format. Example: default")
+	flag.StringVar(&syslogTag, "syslog-tag", "", "syslog tag. Example: sometag.")
+
+	flag.StringVar(&host, "host", "127.0.0.1:22", "host and port for connect. Example: 127.0.0.1:22")
+	flag.StringVar(&user, "user", "admin4eg", "username for connect. Example: admin4eg")
+	flag.StringVar(&password, "password", "pass", "password for connect. Example: pass")
+	flag.StringVar(&shellPrompt, "shell-prompt", "#", "shell prompt for remote terminal. Example: '#'")
+	flag.StringVar(&rawTimeoutForExecuteCommand, "execute-timeout", "5s", "execute timeout. Example: 5s")
+	flag.StringVar(&rawCommands, "commands", "show hostname,show interface mgmt 0 | json", "commands for execute. Delimiter ','. Example: show hostname,show interface mgmt 0 | json")
+	flag.StringVar(&rawMaxBufferBytes, "max-buffer-bytes", "1000", "max buffer bytes for read execute result. Example: 1000")
+	flag.Parse()
+}
+
 func main() {
-	logging, err := logger.NewLogger("stdout", "info", "default", "")
+	logging, err := logger.NewLogger(logOutput, logLevel, logFormat, syslogTag)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	host := "1.1.1.1:22"
-	user := "admin4eg"
-	password := "pass"
-	shellPrompt := "#" //or $ for example
-	timeoutForExecuteCommand := time.Duration(2 * time.Second)
-	commands := []string{"show hostname", "show interface mgmt 0 | json"}
-	var maxBufferBytes uint = 1000
+	timeoutForExecuteCommand, err := time.ParseDuration(rawTimeoutForExecuteCommand)
+	if err != nil {
+		logging.Fatalf("fail to parse timeout for execute command: %v", err)
+	}
+
+	commands := strings.Split(rawCommands, ",")
+
+	u64, err := strconv.ParseUint(rawMaxBufferBytes, 10, 32)
+	if err != nil {
+		logging.Fatalf("fail to parse max-buffer-bytes: %v", err)
+	}
+	maxBufferBytes := uint(u64)
+
+	logging.WithFields(logrus.Fields{
+		"Log output":                  logOutput,
+		"Log level":                   logLevel,
+		"Log format":                  logFormat,
+		"Log:syslog tag":              syslogTag,
+		"Host":                        host,
+		"User":                        user,
+		"Password":                    password,
+		"Shell prompt":                shellPrompt,
+		"Timeout for execute command": timeoutForExecuteCommand,
+		"Commands":                    commands,
+		"Max buffer bytes":            maxBufferBytes,
+	}).Info("Start parameters is:")
 
 	sshClient, err := newSSHClient(host, user, password)
 	if err != nil {
@@ -51,11 +91,7 @@ func newSSHClient(addr, user, password string) (*ssh.Client, error) {
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
-		HostKeyCallback: ssh.HostKeyCallback(func(hostname string,
-			remote net.Addr,
-			key ssh.PublicKey) error {
-			return nil
-		}),
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
 	sshClient, err := ssh.Dial("tcp", addr, sshConfig)
